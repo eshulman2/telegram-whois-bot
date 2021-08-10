264a7c8eab77bd7987f6116d632dad1903427247
Ella Shulman <eshulman@redhat.com>
2021-08-10 20:52:25 +0300

    added ability to generate config from env vars

--- config_bootstrap.py
+++ config_bootstrap.py
@@ -2,6 +2,7 @@
 import hashlib
 import getpass
 import json
+import os
 
 def compare_passwords():
         password = getpass.getpass(prompt="please enter the password"
@@ -14,18 +15,24 @@ def compare_passwords():
 
 
 def main():
-    try:
-        eq = False
-        while not eq:
-            password, eq = compare_passwords()
-            if eq is False:
-                print("passwords doesn't match")
+    if "BOT_PASSWORD" in os.environ:
+        password = os.environ["BOT_PASSWORD"]
+    else:
+        try:
+            eq = False
+            while not eq:
+                password, eq = compare_passwords()
+                if eq is False:
+                    print("passwords doesn't match")
 
-    except Exception as e:
-        raise RuntimeError
+        except Exception as e:
+            raise RuntimeError
     
-    token = input("please enter the token yourecived from BotFather: ")
-
+    if "BOT_TOKEN" in os.environ:
+        token = os.environ["BOT_TOKEN"]
+    else:
+        token = input("please enter the token yourecived from BotFather: ")
+    
     password_hash = hashlib.sha512(password.encode("utf-8")).hexdigest()
 
     configuration = {"token": token, "password": password_hash}
